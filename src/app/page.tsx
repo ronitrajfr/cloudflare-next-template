995324f68549b8c527fe5361cbf4f9ba67ed9b19
ronitrajfr <ronitrajofficial7@gmail.com>
2024-12-04 19:22:12 +0530

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,6 +52,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
+          <p>{process.env.IDK}</p>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Find in-depth information about Next.js features and API.
           </p>
